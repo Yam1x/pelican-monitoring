ca344e422f5e2e1597e2a6f23aed90595479321a
sergiobagd <sbagdasaryan@tourmalincore.com>
2025-08-14 11:24:14 +0500

    chore: leave only lighthouse default assertions

--- .lighthouserc.desktop.js
+++ .lighthouserc.desktop.js
@@ -13,39 +13,7 @@ module.exports = {
       outputDir: '.lighthouseci'
     },
     assert: {
-      assertions: {
-        // Performance
-        'largest-contentful-paint': [
-          'warn', { maxNumericValue: 2400 },
-          'error', { maxNumericValue: 4000 }
-        ],
-        'first-contentful-paint': [
-          'warn', { maxNumericValue: 1800 },
-          'error', { maxNumericValue: 3000 }
-        ],
-        'speed-index': [
-          'warn', { maxNumericValue: 3400 },
-          'error', { maxNumericValue: 5800 }
-        ],
-        'total-blocking-time': [
-          'warn', { maxNumericValue: 150 },
-          'error', { maxNumericValue: 350 }
-        ],
-        'interactive': ['error', { maxNumericValue: 5000 }],
-
-        // Categories
-        'categories:accessibility': ['error', { minScore: 0.9 }],
-
-        // CSP / Security headers (as close as possible)
-        'csp-xss': ['error', { minScore: 1 }], // Available in core audits
-        'uses-http2': 'warn',
-        'is-on-https': 'error',
-        'redirects-http': 'error',
-        'uses-rel-preconnect': 'warn',
-
-        // HSTS – Only "has-hs  ts" is available
-        'has-hsts': ['error', { minScore: 1 }]
+        preset: 'lighthouse:recommended'
       }
     },
-  }
-};
+  };
